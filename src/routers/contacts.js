7d2ec961b379e0cad361e170f0e42fa044290d37
Anatoliy_Noscov <svodnuy7777@gmail.com>
2025-08-18 14:12:36 +0300

    #40

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -8,13 +8,18 @@ import {
   patchContactController,
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import { createContactSchema } from '../validation/contacts.js';
+import { updateContactSchema } from '../validation/contacts.js';
+import { isValidId } from '../middlewares/isValidId.js';
 
 const router = new Router();
 
 router.get('/contacts', ctrlWrapper(getContactsController));
-router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
-router.post('/contacts', ctrlWrapper(createContactController));
+router.get('/contacts/:contactId', isValidId, ctrlWrapper(getContactByIdController));
+router.post('/contacts', validateBody(createContactSchema),
+ctrlWrapper(createContactController));
 router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
-router.patch('/contacts/:contactId', ctrlWrapper(patchContactController));
+router.patch('/contacts/:contactId', validateBody(updateContactSchema),ctrlWrapper(patchContactController));
 
 export default router;
---
+++ src/validation/contacts.js
@@ -0,0 +1,21 @@
+import Joi from 'joi';
+
+
+export const createContactSchema = Joi.object({
+  name: Joi.string().min(3).max(20).required(),
+  age: Joi.number().integer().min(6).max(16).required(),
+  gender: Joi.string().valid('male', 'female', 'other').required(),
+  avgMark: Joi.number().min(2).max(12).required(),
+  onDuty: Joi.boolean(),
+});
+
+
+
+export const updateContactSchema = Joi.object({
+    name: Joi.string().min(3).max(20),
+    email: Joi.string().email(),
+    age: Joi.number().integer().min(6).max(16).required(),
+    gender: Joi.string().valid('male', 'female', 'other').required(),
+    avgMark: Joi.number().min(2).max(12),
+    onDuty: Joi.boolean(),
+  });

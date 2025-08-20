9a25947528d6ceb9cfac8b7beca0812a06b78b73
Anatoliy_Noscov <svodnuy7777@gmail.com>
2025-08-20 23:31:33 +0300

    #49

--- src/db/models/contact.js
+++ src/db/models/contact.js
@@ -1,38 +1,19 @@
 import mongoose from 'mongoose';
 const { Schema, model } = mongoose;
 
-export const contactsSchema = new Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-
-    phone: {
-      type: String,
-      required: true,
-    },
-
-    email: {
-      type: String,
-    },
-
-    favorite: {
-      type: Boolean,
-      default: false,
-    },
-
-    contactType: {
-      type: String,
-      enum: ['work', 'home', 'personal'],
-      required: true,
-      default: 'personal',
-    },
-  },
-  {
-    timestamps: true,
-    versionKey: false,
-  },
-);
+export const contactsSchema = new Schema({
+  name: { type: String, required: true },
+  phone: { type: String, required: true },
+  email: { type: String },
+  isFavorite: { type: Boolean, default: false },
+  contactType: { 
+    type: String, 
+    enum: ['work', 'home', 'personal'], 
+    default: 'personal'
+  }
+}, {
+  timestamps: true, 
+  versionKey: false,
+});
 
 export const ContactsCollection = model('Contacts', contactsSchema);
--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -3,13 +3,11 @@ import Joi from 'joi';
 export const createContactSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
   email: Joi.string().email().required(),
-  phone: Joi.string()
-    .pattern(/^\(\d{3}\) \d{3}-\d{4}$/)
-    .required(),
-  favorite: Joi.boolean(),
-  contactType: Joi.string().valid('work', 'home', 'personal'),
+  phone: Joi.string().pattern(/^\(\d{3}\) \d{3}-\d{4}$/).required(),
+  isFavorite: Joi.boolean(),
+  contactType: Joi.string().valid('work', 'home', 'personal').default('personal') 
 });
 
 export const updateContactSchema = Joi.object({
-  favorite: Joi.boolean().required(),
+  isFavorite: Joi.boolean().required() 
 });

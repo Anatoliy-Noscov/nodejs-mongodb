28ddcf74a9c38a73c519191d445c26d9d3aa4576
Anatoliy_Noscov <svodnuy7777@gmail.com>
2025-08-21 13:57:04 +0300

    #492

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -3,10 +3,15 @@ import Joi from 'joi';
 export const createContactSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
   email: Joi.string().email().required(),
-  phoneNumber: Joi.string().pattern(/^\(\d{3}\) \d{3}-\d{4}$/).required(),
+  phoneNumber: Joi.string().pattern(/^(\+38|8|380)\d{10}$/).required(),
   isFavorite: Joi.boolean(),
   contactType: Joi.string().valid('work', 'home', 'personal').default('personal') 
-});
+},
+{
+  timestamps: true,
+  versionKey: false,
+}
+);
 
 export const updateContactSchema = Joi.object({
   isFavorite: Joi.boolean().required() 
